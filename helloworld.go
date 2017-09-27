fa4f6dda161ae6a7b5674576a5a9462a8e71825e
unknown <>
2017-09-27 21:35:12 +0100

    solution-go

---
+++ helloworld.go
@@ -0,0 +1,8 @@
+package main     // package defined.every project just only have one main package,it is a entrance for the program
+
+import "fmt"  //import statement.fmt is format output
+
+func main()    // it is the entrance of the main function program
+{
+     fmt.Prinln("こんにちは 世界") // output こんにちは世界
+}
